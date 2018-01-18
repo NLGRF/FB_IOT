9aadc201b8ae00da1dba5f032b2d207aa2b8b776
NLGRF <nongrace6654@gmail.com>
2018-01-19 02:37:35 +0700

    Fragment

--- app/src/main/java/com/example/a6654/fb_iot/MainActivity.java
+++ app/src/main/java/com/example/a6654/fb_iot/MainActivity.java
@@ -3,11 +3,25 @@ package com.example.a6654.fb_iot;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.a6654.fb_iot.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+//        Add Fragment
+        if (savedInstanceState == null) {
+
+//            Add Fragment to Activity
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentMainFragment, new MainFragment()).commit();
+
+        }
+
+
+    }   // Main Method
+
+}   // Main Class
---
+++ app/src/main/java/com/example/a6654/fb_iot/fragment/MainFragment.java
@@ -0,0 +1,25 @@
+package com.example.a6654.fb_iot.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.a6654.fb_iot.R;
+
+/**
+ * Created by masterung on 25/11/2017 AD.
+ */
+
+public class MainFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+}   // Main Class
+
